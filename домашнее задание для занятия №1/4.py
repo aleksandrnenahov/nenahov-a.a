4b31343771426688781d27e5a2168a1e0a4af947
Александр <alex1ndrr@mail.ru>
2022-09-13 11:02:41 +0300

    внесены изменения

---
+++ домашнее задание для занятия №1/4.py
@@ -0,0 +1,6 @@
+second = int(input('Введите кол-во секунд: '))
+print('Ваше кол-во секунд в днях: ' + str(round(second / 86400, 2)))
+
+print('Ваше кол-во секунд в часах: ' + str(round(second / 3600, 2)))
+
+print('Ваше кол-во секунд в минутах: ' + str(round(second / 60, 2)))
