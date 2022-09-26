cbe2d96413ec6a0aa2b7cbbcf84dad7ac4d44ab8
Александр <alex1ndrr@mail.ru>
2022-09-26 12:02:26 +0300

    внесены изменения

---
+++ домашнее задание для занятия N2/3.py
@@ -0,0 +1,10 @@
+import math
+x = 3.74 * 10 ** -2
+y = -0.825
+z = 0.16 * 10 ** 2
+
+
+s = (((1 + math.sin(x + y) ** 2) * x ** abs(y)) /\
+abs(x - ((2 * y) / (1 + x ** 2 * y ** 2)))) + math.cos(math.atan(1 / z)) ** 2
+
+print(s)
